743cc2dc46d80875cb1e1cbc34864157b4f2fb60
visethhul <visethhul@gmail.com>
2020-12-26 09:12:21 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'envision',
+    'version': '12.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'envision Development Tutorials',
+    'sequence': '10',
+    'license': 'AGPL-3',
+    'author': 'envision',
+    'maintainer': 'Odoo Mates',
+    'website': 'http://envsion.com',
+    'live_test_url': 'https://www.youtube.com/watch?v=BDepk0LhVuI&list=PLqRRLx0cl0hoJhjFWkFYowveq2Zn55dhM&index=1',
+    'depends': ['sale', 'mail'],
+    'demo': [],
+    'data': ['patient.xml'],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+# Video Explanation: https://www.youtube.com/watch?v=BDepk0LhVuI&list=PLqRRLx0cl0hoJhjFWkFYowveq2Zn55dhM&index=1
---
+++ patient.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class EnvisionPatient(models.Model):
+	_name = 'envision.patient'
+	_description = 'Patient Record'
+
+	patient_name = fields.Char(string='Name',required=True)
+	patient_age = fields.Integer('Age')
+	notes = fields.Text(string='Notes')
+	image = fields.Binary(string='Image')
